f4e5582bc32bf034e3d6b4e56aa3c0b36c2ec9ed
ggouzi <gaetan.gouzi@insa-lyon.fr>
2018-03-04 00:13:47 +0100

    Update frontend

--- frontend/js/json/simple/output200.js
+++ frontend/js/json/simple/output200.js
@@ -1,44 +1,44 @@
 var JSONsimpleOutput200 = {
-	"file": {
-		"file_name": "test.JPG",
-		"file_size": "155 kB",
-		"file_access_date": "2018-03-02 23:53:50 +0100",
-		"file_modify_date": "2018-02-26 22:46:37 +0100",
-		"image_height": "480",
-		"image_width": "640"
-	},
-	"camera": {
-		"make": "NIKON",
-		"model": "COOLPIX P6000",
-		"compression": "JPEG (old-style)",
-		"date_time_original": "2008-10-22 16:29:49 +0200",
-		"megapixels": "0.307",
-		"orientation": "Horizontal (normal)",
-		"software": "Nikon Transfer 1.1 W"
-	},
-	"technical": {
-		"aperture": "4.5",
-		"brightness": null,
-		"contrast": "Normal",
-		"exposure_time": "1/178",
-		"filter_effect": null,
-		"flash": "Off, Did not fire",
-		"flash_setting": null,
-		"f_number": "4.5",
-		"focal_length": "6.0 mm",
-		"iso": "64",
-		"light_source": null,
-		"light_value": "12.5",
-		"max_aperture_value": "2.7",
-		"saturation": "Normal",
-		"shutter_speed": "1/178",
-		"white_balance": "Auto"
-	},
-	"gps": {
-		"gps_date_time": "2008-10-23 14:28:17 UTC",
-		"gps_latitude": "43 deg 28' 1.76\" N",
-		"gps_latitude_ref": "North",
-		"gps_longitude": "11 deg 53' 7.42\" E",
-		"gps_longitude_ref": "East"
-	}
+    "file": {
+        "file_access_date": "2018-03-03T22:50:00+00:00",
+        "file_modify_date": "2018-03-03T17:55:00+00:00",
+        "file_name": "test.JPG",
+        "file_size": "155 kB",
+        "image_height": 480,
+        "image_width": 640
+    },
+    "camera": {
+        "compression": "JPEG (old-style)",
+        "date_time_original": "2008-10-22T16:29:00+00:00",
+        "make": "NIKON",
+        "megapixels": 0.307,
+        "model": "COOLPIX P6000",
+        "orientation": "Horizontal (normal)",
+        "software": "Nikon Transfer 1.1 W"
+    },
+    "technical": {
+        "aperture": 4.5,
+        "brightness": 0,
+        "contrast": "Normal",
+        "exposure_time": "1/178",
+        "f_number": 4.5,
+        "filter_effect": null,
+        "flash": "Off, Did not fire",
+        "flash_setting": null,
+        "focal_length": "6.0 mm",
+        "iso": 64,
+        "light_source": null,
+        "light_value": 12.5,
+        "max_aperture_value": 2.7,
+        "saturation": "Normal",
+        "shutter_speed": "1/178",
+        "white_balance": "Auto"
+    },
+    "gps": {
+        "gps_date_time": "2008-10-23T14:28:00+00:00",
+        "gps_latitude": 43.46716,
+        "gps_latitude_ref": "N",
+        "gps_longitude": 11.88539,
+        "gps_longitude_ref": "E"
+    }
 }
